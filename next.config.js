fae0a007dfc4e90e79d976cb42870fe6ef3b3fa4
athena272 <guilhermera272@gmail.com>
2022-09-15 12:18:07 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['store.storeimages.cdn-apple.com'],
+  },
 }
 
 module.exports = nextConfig
