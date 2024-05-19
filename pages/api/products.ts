c88c95800ffb57736d79335224dd6efb8b8806ff
athena272 <guilhermera272@gmail.com>
2024-05-19 20:51:08 -0300

    starting the project again

--- pages/api/products.ts
+++
@@ -1,8 +0,0 @@
-// pages/api/products.ts
-
-import { NextApiRequest, NextApiResponse } from 'next';
-import products from '../../database.json'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(products)
-}
