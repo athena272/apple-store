5dfb808085470216eddad000721a1d830952dada
athena272 <guilhermera272@gmail.com>
2024-05-19 21:18:24 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head content="Entre na Apple Store e encontre o melhor jeito de comprar o que você ama" />
       <body>
         <Main />
         <NextScript />
