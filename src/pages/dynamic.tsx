b8abc6f068cbeaa10f0836f5d67589d13cec4f13
athena272 <guilhermera272@gmail.com>
2024-05-28 13:08:16 -0300

    Update dynamic.tsx

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -1,7 +1,39 @@
-import { NextPage } from "next"
+// pages/dynamic.tsx
+
+import { GetServerSideProps, NextPage } from "next"
+import { ReactNode, useEffect, useState } from "react"
 import { Col, Container, Row } from "reactstrap"
 
-const Dynamic: NextPage = () => {
+type ApiResponse = {
+  name: string
+  timestamp: Date
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+
+  return {
+    props: {
+      serverSideData
+    }
+  }
+}
+
+const Dynamic: NextPage = (props: {
+  children?: ReactNode
+  serverSideData?: ApiResponse
+}) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("/api/hello").then(res => res.json())
+    setClientSideData(data)
+  }
+
   return (
     <Container tag="main">
       <h1 className="my-5">
@@ -12,12 +44,18 @@ const Dynamic: NextPage = () => {
         <Col>
           <h3>
             Gerado no servidor:
+            <p>
+              {props.serverSideData?.timestamp.toString()}
+            </p>
           </h3>
         </Col>
 
         <Col>
           <h3>
             Gerado no cliente:
+            <p>
+              {clientSideData?.timestamp.toString()}
+            </p>
           </h3>
         </Col>
       </Row>
