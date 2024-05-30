76d48dc69bebb1eb917bfc4ce95876ddbac3ac88
athena272 <guilhermera272@gmail.com>
2024-05-29 23:57:16 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from "next/app";
 import { CartContextProvider } from '@/hooks/useCart';
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartContextProvider>
       <Component {...pageProps} />
+      <Analytics />
     </CartContextProvider>
   );
 }
